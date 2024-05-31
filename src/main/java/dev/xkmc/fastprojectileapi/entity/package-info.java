74cccfbe5bd5b42672491de65db7be14e82dcd77
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-05-30 20:09:17 -0500

    port engine

---
+++ src/main/java/dev/xkmc/fastprojectileapi/entity/package-info.java
@@ -0,0 +1,8 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+
+package dev.xkmc.fastprojectileapi.entity;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ src/main/java/dev/xkmc/l2magic/content/particle/package-info.java
@@ -0,0 +1,8 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+
+package dev.xkmc.l2magic.content.particle;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
