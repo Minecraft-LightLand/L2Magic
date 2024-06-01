20ffa1d49e90ca324b92089c99c8e2050c9fcd0c
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-05-31 22:21:29 -0400

    fix reg

--- src/main/java/dev/xkmc/l2magic/content/engine/iterator/SphereRandomIterator.java
+++ src/main/java/dev/xkmc/l2magic/content/engine/iterator/SphereRandomIterator.java
@@ -29,7 +29,7 @@ public record SphereRandomIterator(DoubleVariable radius, IntVariable count,
 
 	@Override
 	public EngineType<SphereRandomIterator> type() {
-		return EngineRegistry.RANDOM_FAN.get();
+		return EngineRegistry.RANDOM_SPHERE.get();
 	}
 
 	@Override
