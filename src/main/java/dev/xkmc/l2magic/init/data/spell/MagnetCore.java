28708152da7048ac4978a1ab4b95cbb171114b9c
小鸟同学@提不起劲 <1438726022@qq.com>
2024-06-03 15:32:35 +0800

    update particles

--- src/main/java/dev/xkmc/l2magic/init/data/spell/MagnetCore.java
+++ src/main/java/dev/xkmc/l2magic/init/data/spell/MagnetCore.java
@@ -3,26 +3,15 @@ package dev.xkmc.l2magic.init.data.spell;
 import com.tterrag.registrate.providers.RegistrateLangProvider;
 import dev.xkmc.l2magic.content.engine.context.DataGenContext;
 import dev.xkmc.l2magic.content.engine.core.ConfiguredEngine;
-import dev.xkmc.l2magic.content.engine.iterator.DelayedIterator;
-import dev.xkmc.l2magic.content.engine.iterator.LoopIterator;
-import dev.xkmc.l2magic.content.engine.iterator.SphereRandomIterator;
-import dev.xkmc.l2magic.content.engine.logic.DelayLogic;
-import dev.xkmc.l2magic.content.engine.logic.ListLogic;
-import dev.xkmc.l2magic.content.engine.logic.ProcessorEngine;
-import dev.xkmc.l2magic.content.engine.particle.DustParticleInstance;
-import dev.xkmc.l2magic.content.engine.particle.SimpleParticleInstance;
-import dev.xkmc.l2magic.content.engine.processor.DamageProcessor;
-import dev.xkmc.l2magic.content.engine.processor.PushProcessor;
-import dev.xkmc.l2magic.content.engine.selector.BoxSelector;
-import dev.xkmc.l2magic.content.engine.selector.SelectionType;
-import dev.xkmc.l2magic.content.engine.spell.SpellAction;
-import dev.xkmc.l2magic.content.engine.spell.SpellCastType;
-import dev.xkmc.l2magic.content.engine.spell.SpellTriggerType;
-import dev.xkmc.l2magic.content.engine.variable.ColorVariable;
-import dev.xkmc.l2magic.content.engine.variable.DoubleVariable;
-import dev.xkmc.l2magic.content.engine.variable.IntVariable;
+import dev.xkmc.l2magic.content.engine.iterator.*;
+import dev.xkmc.l2magic.content.engine.logic.*;
+import dev.xkmc.l2magic.content.engine.modifier.*;
+import dev.xkmc.l2magic.content.engine.particle.*;
+import dev.xkmc.l2magic.content.engine.processor.*;
+import dev.xkmc.l2magic.content.engine.selector.*;
+import dev.xkmc.l2magic.content.engine.spell.*;
+import dev.xkmc.l2magic.content.engine.variable.*;
 import dev.xkmc.l2magic.init.data.SpellDataGenEntry;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.damagesource.DamageTypes;
@@ -49,102 +38,106 @@ public class MagnetCore extends SpellDataGenEntry {
 		).verifyOnBuild(ctx, MAGNET_CORE);
 	}
 
-	private static ConfiguredEngine<?> magnetCore(DataGenContext ctx) {
-		return new ListLogic(List.of(
-				new DelayedIterator(
-						IntVariable.of("55"),
-						IntVariable.of("2"),
-						new SphereRandomIterator(
-								DoubleVariable.of("0.2+0.01*r"),
-								IntVariable.of("20"),
-								new DustParticleInstance(
-										ColorVariable.Static.of(0xE88B00),
-										DoubleVariable.of("2+0.04*r"),
-										DoubleVariable.of("0.1"),
-										IntVariable.of("10")
-								),
-								null
-						),
-						"r"
-				),
-				new LoopIterator(
-						IntVariable.of("5"),
-						new DelayLogic(
-								IntVariable.of("10+30*t-2*t*t"),
-								new SphereRandomIterator(
-										DoubleVariable.of("8"),
-										IntVariable.of("200"),
-										new SimpleParticleInstance(
-												ParticleTypes.SQUID_INK,
-												DoubleVariable.of("-.5")
-										),
-										null
-								)
-						),
-						"t"
-				),
-				new DelayLogic(
-						IntVariable.of("115"),
-						new ListLogic(List.of(
-								new SphereRandomIterator(
-										DoubleVariable.of(".01"),
-										IntVariable.of("200"),
-										new SimpleParticleInstance(
-												ParticleTypes.SQUID_INK,
-												DoubleVariable.of("3")
-										),
-										null
-								),
-								new ProcessorEngine(
-										SelectionType.ENEMY,
-										new BoxSelector(
-												DoubleVariable.of("8"),
-												DoubleVariable.of("8"),
-												true
-										),
-										List.of(
-												new DamageProcessor(
-														ctx.damage(DamageTypes.INDIRECT_MAGIC),
-														DoubleVariable.of("20"),
-														true, false
-												),
-												new PushProcessor(
-														DoubleVariable.of(".5"),
-														DoubleVariable.ZERO,
-														DoubleVariable.ZERO,
-														PushProcessor.Type.TO_CENTER
-												)
-										)
-								)
-						))
-				),
-				new DelayedIterator(
-						IntVariable.of("115"),
-						IntVariable.of("1"),
-						new ProcessorEngine(
-								SelectionType.ENEMY,
-								new BoxSelector(
-										DoubleVariable.of("8"),
-										DoubleVariable.of("8"),
-										true
-								),
-								List.of(
-										new DamageProcessor(
-												ctx.damage(DamageTypes.INDIRECT_MAGIC),
-												DoubleVariable.of(".1"),
-												true, false
-										),
-										new PushProcessor(
-												DoubleVariable.of("-.05"),
-												DoubleVariable.ZERO,
-												DoubleVariable.ZERO,
-												PushProcessor.Type.TO_CENTER
-										)
-								)
-						),
-						null
-				)
-		));
-	}
+    private static ConfiguredEngine<?> magnetCore(DataGenContext ctx) {
+        return new ListLogic(List.of(
+                new DelayedIterator(  // core
+                        IntVariable.of("24"),
+                        IntVariable.of("5"),
+                        new SphereRandomIterator(
+                                DoubleVariable.of("0.1+0.005*r"),
+                                IntVariable.of("50"),
+                                new DustParticleInstance(
+                                        ColorVariable.Static.of(0xE88B00),
+                                        DoubleVariable.of("2"),
+                                        DoubleVariable.of(".01"),
+                                        IntVariable.of("10")
+                                ),
+                                null
+                        ),
+                        "r"
+                ),
+                new LoopIterator(  // waves
+                        IntVariable.of("6"),
+                        new DelayLogic(
+                                IntVariable.of("10+30*t-2*t*t"),  // 10 + (0-100), 6 waves
+                                new SphereRandomIterator(
+                                        DoubleVariable.of("8"),
+                                        IntVariable.of("200"),
+                                        new DustParticleInstance(
+                                                ColorVariable.Static.of(0xE88B00),
+                                                DoubleVariable.of("2"),
+                                                DoubleVariable.of("-.5"),
+                                                IntVariable.of("20-2*t")
+                                        ),
+                                        null
+                                )
+                        ),
+                        "t"
+                ),
+                new DelayLogic(  // explode
+                        IntVariable.of("120"),
+                        new ListLogic(List.of(
+                                new SphereRandomIterator(
+                                        DoubleVariable.of(".01"),
+                                        IntVariable.of("200"),
+                                        new DustParticleInstance(
+                                                ColorVariable.Static.of(0xE88B00),
+                                                DoubleVariable.of("2"),
+                                                DoubleVariable.of("3"),
+                                                IntVariable.of("10")
+                                        ),
+                                        null
+                                ),
+                                new ProcessorEngine(
+                                        SelectionType.ENEMY,
+                                        new BoxSelector(
+                                                DoubleVariable.of("8"),  // Only have range explode
+                                                DoubleVariable.of("8"),
+                                                true
+                                        ),
+                                        List.of(
+                                                new DamageProcessor(
+                                                        ctx.damage(DamageTypes.INDIRECT_MAGIC),
+                                                        DoubleVariable.of("20"),
+                                                        true, false
+                                                ),
+                                                new PushProcessor(
+                                                        DoubleVariable.of("1"),
+                                                        DoubleVariable.ZERO,
+                                                        DoubleVariable.ZERO,
+                                                        PushProcessor.Type.TO_CENTER
+                                                )
+                                        )
+                                )
+                        ))
+                ),
+                new DelayedIterator(  // tick damage
+                        IntVariable.of("120"),
+                        IntVariable.of("1"),
+                        new ProcessorEngine(
+                                SelectionType.ENEMY,
+                                new BoxSelector(
+                                        DoubleVariable.of("16"),  // full range drain
+                                        DoubleVariable.of("16"),
+                                        true
+                                ),
+                                List.of(
+                                        new DamageProcessor(
+                                                ctx.damage(DamageTypes.INDIRECT_MAGIC),
+                                                DoubleVariable.of("1"),
+                                                true, false
+                                        ),
+                                        new PushProcessor(
+                                                DoubleVariable.of("-.1"),
+                                                DoubleVariable.ZERO,
+                                                DoubleVariable.ZERO,
+                                                PushProcessor.Type.TO_CENTER
+                                        )
+                                )
+                        ),
+                        null
+                )
+        ));
+    }
 
 }
--- src/main/java/dev/xkmc/l2magic/init/data/spell/MasterSpark.java
+++ src/main/java/dev/xkmc/l2magic/init/data/spell/MasterSpark.java
@@ -3,23 +3,14 @@ package dev.xkmc.l2magic.init.data.spell;
 import com.tterrag.registrate.providers.RegistrateLangProvider;
 import dev.xkmc.l2magic.content.engine.context.DataGenContext;
 import dev.xkmc.l2magic.content.engine.core.ConfiguredEngine;
-import dev.xkmc.l2magic.content.engine.iterator.RingRandomIterator;
-import dev.xkmc.l2magic.content.engine.iterator.SphereRandomIterator;
+import dev.xkmc.l2magic.content.engine.iterator.*;
 import dev.xkmc.l2magic.content.engine.logic.*;
-import dev.xkmc.l2magic.content.engine.modifier.Dir2NormalModifier;
-import dev.xkmc.l2magic.content.engine.modifier.OffsetModifier;
-import dev.xkmc.l2magic.content.engine.modifier.RotationModifier;
-import dev.xkmc.l2magic.content.engine.particle.SimpleParticleInstance;
-import dev.xkmc.l2magic.content.engine.processor.DamageProcessor;
-import dev.xkmc.l2magic.content.engine.processor.KnockBackProcessor;
-import dev.xkmc.l2magic.content.engine.selector.LinearCubeSelector;
-import dev.xkmc.l2magic.content.engine.selector.SelectionType;
-import dev.xkmc.l2magic.content.engine.spell.SpellAction;
-import dev.xkmc.l2magic.content.engine.spell.SpellCastType;
-import dev.xkmc.l2magic.content.engine.spell.SpellTriggerType;
-import dev.xkmc.l2magic.content.engine.variable.BooleanVariable;
-import dev.xkmc.l2magic.content.engine.variable.DoubleVariable;
-import dev.xkmc.l2magic.content.engine.variable.IntVariable;
+import dev.xkmc.l2magic.content.engine.modifier.*;
+import dev.xkmc.l2magic.content.engine.particle.*;
+import dev.xkmc.l2magic.content.engine.processor.*;
+import dev.xkmc.l2magic.content.engine.selector.*;
+import dev.xkmc.l2magic.content.engine.spell.*;
+import dev.xkmc.l2magic.content.engine.variable.*;
 import dev.xkmc.l2magic.init.data.SpellDataGenEntry;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.data.worldgen.BootstapContext;
@@ -51,62 +42,64 @@ public class MasterSpark extends SpellDataGenEntry {
 	private static ConfiguredEngine<?> masterSpark(DataGenContext ctx) {
 		return new ListLogic(
 				List.of(
-						new PredicateLogic(
-								BooleanVariable.of("TickUsing<=40"),
+						new PredicateLogic(  // charge
+								BooleanVariable.of("TickUsing<=25"),
 								new SphereRandomIterator(
 										DoubleVariable.of("3"),
-										IntVariable.of("20"),
-										new SimpleParticleInstance(
-												ParticleTypes.END_ROD,
-												DoubleVariable.of("-.2")
+										IntVariable.of("10"),
+										new DustParticleInstance(
+												ColorVariable.Static.of(0xFFFFFF),
+												DoubleVariable.of("1"),
+												DoubleVariable.of("-.2"),
+												IntVariable.of("10")
 										),
 										null
 								),
 								null
 						),
-						new DelayLogic(
-								IntVariable.of("60"),
-								new ListLogic(
-										List.of(
-												new ProcessorEngine(SelectionType.ENEMY,
-														new LinearCubeSelector(
-																IntVariable.of("8"), // dist=16, r=2
-																DoubleVariable.of("2")
-														),
-														List.of(
-																new DamageProcessor(
-																		ctx.damage(DamageTypes.INDIRECT_MAGIC),
-																		DoubleVariable.of("10"),
-																		true, true
-																),
-																KnockBackProcessor.of("1")
-														)
+						new PredicateLogic(
+								BooleanVariable.of("TickUsing>50"),
+								new ListLogic(List.of(
+										new ProcessorEngine(SelectionType.ENEMY,
+												new LinearCubeSelector(
+														IntVariable.of("6"), // dist=18, r=3
+														DoubleVariable.of("3")
 												),
-												new MoveEngine(
-														List.of(
-																OffsetModifier.of("0", "-2", "0"),
-																new Dir2NormalModifier()
+												List.of(
+														new DamageProcessor(
+																ctx.damage(DamageTypes.INDIRECT_MAGIC),
+																DoubleVariable.of("10"),
+																true, true
 														),
-														new RingRandomIterator(
-																DoubleVariable.ZERO,
-																DoubleVariable.of("2"),
-																DoubleVariable.of("-180"),
-																DoubleVariable.of("180"),
-																IntVariable.of("10"),
-																new RandomVariableLogic("r", 1,
-																		new MoveEngine(List.of(RotationModifier.of("0", "105-30*r0")),
-																				new SimpleParticleInstance(
-																						ParticleTypes.END_ROD,
-																						DoubleVariable.of("2")
-																				)
-																		)
-																),
-																null
-														)
+														KnockBackProcessor.of("1")
 												)
-
-										)
-								)
+										),
+										new MoveEngine(
+												List.of(
+														OffsetModifier.of("0", "-3", "0"),
+														new Dir2NormalModifier()
+												),
+												new RingRandomIterator(
+														DoubleVariable.ZERO,
+														DoubleVariable.of("3"),
+														DoubleVariable.of("-180"),
+														DoubleVariable.of("180"),
+														IntVariable.of("20"),
+														new RandomVariableLogic("r", 4,
+																new MoveEngine(List.of(RotationModifier.of("0", "105-30*r0")),
+																		new DustParticleInstance(
+																				ColorVariable.Static.of(0xFFFFFF),
+																				DoubleVariable.of("4+4*r1"),
+																				DoubleVariable.of("2+2*r2"),
+																				IntVariable.of("10+10*r3")
+																		)
+																)
+														),
+														null
+												)
+										))
+								),
+								null
 						)
 				)
 		);
